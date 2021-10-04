3e0a9065b9771e28ccae3ab2fe472af544e2350d
souravrane <sourav1121997@gmail.com>
2021-10-04 17:56:41 +0530

    avoiding side effects

---
+++ PureFunctions.js
@@ -0,0 +1,15 @@
+const array = [1, 2, 3];
+
+function removeLastItem(arr) {
+  const newArray = [].concat(arr);
+  newArray.pop();
+  return newArray;
+}
+
+function multiplyBy2(arr) {
+  return arr.map((item) => item * 2);
+}
+
+console.log(removeLastItem(array));
+console.log(multiplyBy2(array));
+console.log(array);
