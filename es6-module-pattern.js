f680cd94759121d7eea040f08b6254d9cabc8b49
souravrane <sourav1121997@gmail.com>
2021-10-09 13:06:47 +0530

    es6-module-pattern

---
+++ es6-module-pattern.js
@@ -0,0 +1,8 @@
+const harry = "Potter";
+const voldermort = "Voldermort";
+
+export function fight(player1, player2) {
+  const score1 = Math.floor(Math.random() * player1.length);
+  const score2 = Math.floor(Math.random() * player2.length);
+  return score1 > score2 ? `${player1} wins!` : `${player2} wins`;
+}
